87ab5388dab0f8b30a9a5f945d95eb275ed709c6
PillowSoPaw <amadorable.angelo@gmail.com>
2016-03-08 16:55:48 +0800

    Created objects: - User - Dog

---
+++ Doggie/app/src/main/java/com/example/justin/doggie/Objects/Dog.java
@@ -0,0 +1,12 @@
+package com.example.justin.doggie.Objects;
+
+/**
+ * Created by Angelo Amadora on 3/8/2016.
+ */
+public class Dog {
+    public String name;
+    public boolean isStray;
+    public int mainPic;
+    public String description;
+
+}
---
+++ Doggie/app/src/main/java/com/example/justin/doggie/Objects/User.java
@@ -0,0 +1,12 @@
+package com.example.justin.doggie.Objects;
+
+/**
+ * Created by Angelo Amadora on 3/8/2016.
+ */
+public class User {
+
+    public String name;
+
+    //These are just temporary datatypes for the variables
+    public String location;
+}
