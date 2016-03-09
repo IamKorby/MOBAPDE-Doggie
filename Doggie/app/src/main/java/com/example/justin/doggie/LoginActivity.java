666656598bfa24db0f7e2027f2a7a03f2cd2ede5
PillowSoPaw <amadorable.angelo@gmail.com>
2016-03-09 14:08:59 +0800

    Changed styles.xml to project's brown theme

--- Doggie/app/src/main/java/com/example/justin/doggie/LoginActivity.java
+++ Doggie/app/src/main/java/com/example/justin/doggie/LoginActivity.java
@@ -1,15 +1,31 @@
 package com.example.justin.doggie;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity
 {
+    Button btnFake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // a button to simulate temporary login
+            btnFake = (Button) findViewById(R.id.tempLogin);
+            btnFake.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent();
+                    i.setClass(getBaseContext(),AdoptionActivity.class);
+                    startActivity(i);
+                }
+            });
+
     }
 }
