d802ef4ddc29a1f16a9eed6586c44d43f45e510b
PillowSoPaw <amadorable.angelo@gmail.com>
2016-03-09 13:12:26 +0800

    Created "drawable" folder due to lack of folder
    Removed app toolbar from LoginActivity/activity_login.xml
    
    Uploaded loginBackground.jpg to drawable folder

--- Doggie/app/src/main/java/com/example/justin/doggie/AdoptionActivity.java
+++ Doggie/app/src/main/java/com/example/justin/doggie/AdoptionActivity.java
@@ -13,10 +13,6 @@ public class AdoptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adoption);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
     }
 
 }
