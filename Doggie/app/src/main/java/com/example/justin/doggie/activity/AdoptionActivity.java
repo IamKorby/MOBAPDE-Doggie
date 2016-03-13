acca1333ac67494ed74fe3d59f7c10c7ec209a65
PillowSoPaw <amadorable.angelo@gmail.com>
2016-03-13 16:49:29 +0800

    Merged both codes.

--- Doggie/app/src/main/java/com/example/justin/doggie/activity/AdoptionActivity.java
+++ Doggie/app/src/main/java/com/example/justin/doggie/activity/AdoptionActivity.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.justin.doggie.Objects.Dog;
 import com.example.justin.doggie.R;
+import com.example.justin.doggie.adapter.DogAdapter;
+
+import java.util.ArrayList;
 
 public class AdoptionActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<Dog> dogs = new ArrayList<>();
+    DogAdapter dogAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adoption);
+
+        //start of code
+        recyclerView = (RecyclerView) findViewById(R.id.rvAdoption);
+        dogs.add(new Dog("Elizabeth",true,"Taft","09178889999"));
+        dogs.add(new Dog("Kerrbie",false,"Makati","09178880000"));
+        dogs.add(new Dog("AJ",false,"Paranaque","09178865800"));
+
+        dogAdapter = new DogAdapter(dogs);
+
+        recyclerView.setAdapter(dogAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
     }
 
 }
--- Doggie/app/src/main/java/com/example/justin/doggie/adapter/DogAdapter.java
+++ Doggie/app/src/main/java/com/example/justin/doggie/adapter/DogAdapter.java
@@ -1,39 +1,84 @@
 package com.example.justin.doggie.adapter;
 
+import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.justin.doggie.Objects.Dog;
+import com.example.justin.doggie.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Angelo Amadora on 3/11/2016.
  */
-public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogHolder>
-{
 
-    @Override
-    public DogHolder onCreateViewHolder(ViewGroup parent, int viewType)
-    {
-        return null;
-    }
 
-    @Override
-    public void onBindViewHolder(DogHolder holder, int position)
-    {
+    public class DogAdapter extends RecyclerView.Adapter<DogAdapter.DogHolder> {
+        ArrayList<Dog> dogs;
 
-    }
-
-    @Override
-    public int getItemCount()
-    {
-        return 0;
-    }
-
-    public class DogHolder extends RecyclerView.ViewHolder
-    {
-
-        public DogHolder(View itemView)
-        {
-            super(itemView);
+        public DogAdapter(ArrayList<Dog> dogs) {
+            //arraylist of dogs gets passed here
+            this.dogs = dogs;
         }
+
+        @Override
+        public DogHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //attaches the dog_item.xml
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.dog_item, null);
+            return new DogHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(DogHolder holder, int position) {
+//        String name = dogs.get(position);
+//        String location;
+//        Image owner;
+//        Image dog;
+//        String contact;
+//        boolean hasPapers;
+
+
+            Dog temp = dogs.get(position);
+
+            holder.tvDogName.setText(temp.name);
+            holder.tvDogLocation.setText(temp.location);
+            holder.tvOwnerContact.setText(temp.contact);
+
+            if (temp.isStray) {
+                holder.ivHasPapers.setVisibility(View.INVISIBLE);
+            }
+        }
+
+
+            public int getItemCount() {
+                return dogs.size();
+            }
+
+            public class DogHolder extends RecyclerView.ViewHolder {
+                TextView tvDogName;
+                TextView tvDogLocation;
+
+                CircleImageView civOwnerPicture;
+                ImageView ivDogPicture;
+                ImageView ivHasPapers;
+                TextView tvOwnerContact;
+
+                public DogHolder(View itemView) {
+                    super(itemView);
+                    tvDogName = (TextView) itemView.findViewById(R.id.tvDogName);
+                    tvDogLocation = (TextView) itemView.findViewById(R.id.tvDogLocation);
+                    civOwnerPicture = (CircleImageView) itemView.findViewById(R.id.civOwnerPic);
+                    ivDogPicture = (ImageView) itemView.findViewById(R.id.ivDogPicture);
+                    ivHasPapers = (ImageView) itemView.findViewById(R.id.ivHasPapers);
+                    tvOwnerContact = (TextView) itemView.findViewById(R.id.tvOwnerNo);
+                }
+            }
+
     }
-}
